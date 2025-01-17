aac74beece28261d13ca009b5078ab749f43b0e8
jonas portugall <jonascportugal30@gmail.com>
2025-01-17 17:43:43 +0100

    acçao de update da tarefa adicionado

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('hellowold/', views.hellowold),
     path('',views.taskList, name='task-list'),
     path('task/<int:id>', views.showTask, name="show-task"),
-    path('yourname/<str:name>',views.yourName, name='your-name')
+    path('yourname/<str:name>',views.yourName, name='your-name'),
+    path('newTask/',views.newTask, name="new-task"),
+    path('edit/<int:id>',views.editTask, name="edit-task"),
+    path('delete/<int:int>',views.deleteTask,name="delete-task")
 ]
+
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
@@ -9,7 +10,7 @@ def hellowold(request):
     return HttpResponse('HelloWord')
 
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/list.html',{'tasks' : tasks } )
 
 def yourName(request , name):
@@ -18,3 +19,35 @@ def yourName(request , name):
 def showTask(request, id):
     task = get_object_or_404(Task , pk=id)
     return render(request , 'tasks/showtask.html',{'task':task})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+        return render(request , 'tasks/addTask.html',{'form':form})
+
+def deleteTask(request):
+    return false
+
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+    #se for post entao estah a fazer o update
+    if(request.method == 'POST'):
+        form = TaskForm(request.POST , instance=task)
+        if(form.is_valid()):
+            task.save()
+            return redirect('/')
+        else:
+            return render(request , 'tasks/editTask.html', {'form':form, 'task':task})    
+    
+    #senao, ainda estamos no edit
+    else:
+        return render(request , 'tasks/editTask.html', {'form':form, 'task':task})
+
